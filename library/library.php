c703f8cb91a8ca61e34dbd34a8be556baf107440
VanSickle-Wendi <gwendells@gmail.com>
2015-11-25 14:11:38 -0700

    Finished JSON Issues
    I'm committing Amy's recent changes to the 2-F15-CIT261 repository

--- library/library.php
+++ library/library.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <head>
         <title>Library</title>
@@ -10,10 +10,11 @@
 
       <p id="test1"></p>
       <p id="test2"></p>
+      <p id="test3"></p>
 <script>
 
 var text;
-var arr;
+var arr = [];
 
 // A movie object
 function Movie(title, year) {
@@ -38,11 +39,37 @@ movie2.display();
 arr.push(movie1);
 arr.push(movie2);
 
+
+// For testing purposes:
 // Stringify a created object
-text = JSON.stringify(movie1);
+text = JSON.stringify(arr);
 document.getElementById("test2").innerHTML = text;
 
 
+// Test for local storage
+if (typeof(Storage) !== "undefined")
+{
+  // TODO: Put all local storage code here
+}
+else 
+{
+  document.getElementById("results1").innerHTML = "Sorry, Local Storage is not supported by your browser";
+}
+
+// Put the array in local storage
+localStorage.setItem("movieArray", text);
+
+// Get the array from local storage
+var object = localStorage.getItem("movieArray");
+// ***NOTE: obj is basically arr from above
+var obj = JSON.parse(object);
+
+var test = "";
+for (var i = 0; i < obj.length; i++) {
+  test += obj[i].title + "<br/>";
+}
+
+document.getElementById("test3").innerHTML = test;
 
 </script>
 
