fbf1558e8082788fde6959fae2fb88fb78de2282
VanSickle-Wendi <gwendells@gmail.com>
2015-11-28 02:03:00 -0700

    Created test files for testing a homepage
    I created a page called Test Home, so I can work on a layout for the
    home page without messing up someone else's work. All associated files
    or folders have the word "test" either in the name of the file or the
    folder.

--- modules/nav.php
+++ modules/nav.php
@@ -1,5 +1,6 @@
       <ul id="nav">
         <li><a href="/index.php">Home</a></li>
+        <li><a href="/test_home_page/home_page.php">Test Home</a></li>
         <li><a href="/forms/add_movies.php">Add Movies</a></li>
         <li><a href="/library/library.php">Movie Library</a></li>
         <li><input type="text" placeholder="Search" ></li> 
