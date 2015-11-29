40bc659622748c1e1916d55a6ced83a91eb8bc2e
VanSickle-Wendi <gwendells@gmail.com>
2015-11-28 22:59:35 -0700

    Still working with test pages
    Created a json_test page. Trying to figure out the layout for the home
    page and where the code to hide divs should go.

--- modules/forms_test/add_movies.php
+++ modules/forms_test/add_movies.php
@@ -1,26 +1,36 @@
-        <form>
-            <p>Title</p>
-            <input type="text" name="title" size="30" maxlength="50"/><br>
-            <p>Star</p>
-            <input type="text" name="star" size="30" maxlength="50"/><br>
-            <p>Genre</p>
-            <select name="genre">
-                <option value="blank"></option> 
-                <option value="action">Action</option>                
-                <option value="comedy">Comedy</option>
-                <option value="drama">Drama</option>
-                <option value="horror">Horror</option>
-                <option value="romance">Romance</option>
-            </select>
-            <p>Rating</p>
-            <select name="genre">
-                <option value="blank"></option> 
-                <option value="g">G</option>                
-                <option value="pg">PG</option>
-                <option value="pg">PG-13</option>                
-                <option value="r">R</option>
-                <option value="notRated">Not Rated</option>
-            </select>
-        </form>
+            <form method="post" action="json/json_test.json" id="addMovieForm">
+         <!--       <fieldset>
+                    <legend>
+                        Add New Movie:
+                    </legend> -->
+                    <label for="title">Title:</label><br>
+                    <input type="text" name="title" id="title" size="40"><br>
+                    
+                    <label for="star">Star:</label><br>
+                    <input type="text" name="star" id="star" size="40"><br>
+                    
+                    <label for="genre">Genre:</label><br>
+                    <select name="genre">
+                       <option value="blank"></option> 
+                       <option value="action">Action</option>                
+                       <option value="comedy">Comedy</option>
+                       <option value="drama">Drama</option>
+                       <option value="horror">Horror</option>
+                       <option value="romance">Romance</option>
+                    </select><br>
+                    
+                    <label for="rating">Rating:</label><br>
+                    <select name="genre">
+                       <option value="blank"></option> 
+                       <option value="g">G</option>                
+                       <option value="pg">PG</option>
+                       <option value="pg">PG-13</option>                
+                       <option value="r">R</option>
+                       <option value="notRated">Not Rated</option>
+                    </select><br><br>
+ 
+                    <button type="button" onclick="">Submit</button><br>
+               <!-- </fieldset> -->
+            </form><br>
 
 
--- test_home_page/home_page.php
+++ test_home_page/home_page.php
@@ -6,32 +6,39 @@
     <link href="/css/styletest.css" type="text/css" rel="stylesheet" media="screen"/>
   </head>
   <body>
-    <div class="container">
+   <div class="container">
     <h1 class="title">Movie Library</h1>   
     <!-- Navigation ------------------------------------------------>
     <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/nav.php'; ?>
     
     <div class="mainContent">
-    <!-- Content for viewing Movie Library -------------------------->
-       <div class="movieLibrary">
-          <h3>Movie Library</h3> 
-          <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/library_test/library.php'; ?>     
-       </div>
-    <!-- Content for adding new movies ------------------------------>
-       <div class="addMovie">
-          <h3>Add Movies</h3>
-          <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/forms_test/add_movies.php'; ?>  
-       </div>
-         
-    <!-- Content for Search results ---------------------------------->
+     <!-- Content for viewing Movie Library -------------------------->
+     <div id="movieLibrary">
+       <h3>Movie Library</h3> 
+       <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/library_test/library.php'; ?>        <script>
+           function clearDivs() {
+              document.getElementById('addMovie').style.display = "none";
+              document.getElementById('searchResults').style.display = "none";
+           }
+       </script>    
+     </div>
+     <!-- Content for Adding New Movies ------------------------------>
+     <div id="addMovie">
+       <h3>Add Movies</h3>
+       <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/forms_test/add_movies.php'; ?>
+     </div>
     
+     <!-- Content for Search results ---------------------------------->
+     <div id="searchResults">
+       <h3>Search Results</h3>
+        
+     </div>
          
          
-         
-     </div> <!-- End mainContent -->
-    </div> <!-- End Container -->        
+    </div> <!-- End mainContent -->
+   </div> <!-- End Container -->        
     <!-- Ensuring that Javascript runs ------------------------------->
-        <script type="text/javascript" src="/javascript/javascript_test.js"></script>  
+    <script type="text/javascript" src="/javascript/javascript_test.js"></script>  
 </body>
 </html>
 
