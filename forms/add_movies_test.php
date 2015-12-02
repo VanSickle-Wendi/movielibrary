1abb59bb7937641f95ea3f16967665c4dae1fae7
VanSickle-Wendi <gwendells@gmail.com>
2015-12-02 02:08:51 -0700

    Making Add Movies form on Test Home responsive add CSS
    I built the form based on Tiffany's table form, but  without the table.
    It was easier to make it small enough for a phone without the table.
    
    I added CSS based on Amy's CSS for the table form.

--- forms/add_movies_test.php
+++ forms/add_movies_test.php
@@ -1,10 +1,11 @@
-            <form method="post" action="json/json_test.json" id="addMovieForm">
-
+    <link href="/css/styletest.css" type="text/css" rel="stylesheet" media="screen"/>
+<!-- Made form more responsive based on Tiffany's (table) form -->        
+<form method="post" action="json/json_test.json" id="addMoviesForm">
                     <label for="title">Title:</label><br>
-                    <input type="text" name="title" id="title" size="40"><br>
+                    <input type="text" name="title" id="title" size="35"><br>
                     
                     <label for="star">Star:</label><br>
-                    <input type="text" name="star" id="star" size="40"><br>
+                    <input type="text" name="star" id="star" size="35"><br>
                     
                     <label for="genre">Genre:</label><br>
                     <select name="genre">
@@ -27,6 +28,31 @@
                     </select><br><br>
  
                     <button type="button" onclick="">Submit</button><br>
-            </form><br>
-
-
+</form><br>
+            
+<!--  Adding Amy's (table) style to addMovies form and making responsive-->
+<style>
+/* Form style for Phone --------------------------------------   */ 
+@media only screen and (max-width: 600px) {   
+form {
+	border: 1px solid #069;
+	background-color: #eeeeee;
+	padding: 10px;  
+	color: #069;   
+	width: 300px;        
+} 
+}
+/*............Styling for the tablet/desktop Navigation Here.........*/
+@media only screen and (min-width: 601px) {
+form {
+	border: 1px solid #069;
+	background-color: #eeeeee;
+	padding: 20px;  
+	color: #069;   
+	width: 50%;        
+}
+label {
+        font-size: 1.2em;
+}
+}
+</style>
