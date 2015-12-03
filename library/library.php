e522184f5f90b234c2884b1a789ce4b5e7e8277e
Moore-Brendon <brendonmoore@gmail.com>
2015-12-03 17:28:06 -0600

    Sorting complete

--- library/library.php
+++ library/library.php
@@ -20,40 +20,42 @@
 <form class="browseform">
     <p>Sort:</p>
     <div class="tg-button">
-        <input type="radio" name="tg" id="button1" checked>
+        <input type="radio" name="tg" id="button1" onchange="loadMovies()" checked>
         <label for="button1" unselectable>Alpha</label>
     </div>
     <div class="tg-button">
-        <input type="radio" name="tg" id="button2">
+        <input type="radio" name="tg" id="button2" onchange="loadMovies()">
         <label for="button2" unselectable>Date</label>
     </div>
     <p>  | Filter by:</p>
 <select id="userRating" onchange="loadMovies()">
     
-          <option value="" disabled selected hidden>Rating</option>
-          <option value="g">G</option>
-          <option value="pg">PG</option>
-          <option value="pg-13">PG-13</option>
+          <option value="Rating">Rating</option>
+          <option value="G">G</option>
+          <option value="PG">PG</option>
+          <option value="PG-13">PG-13</option>
           <option value="R">R</option>
-          <option value="not Rated">Not Rated</option>    
+          <option value="Not Rated">Not Rated</option>    
           
         </select> 
 
 <select id="userGenre" onchange="loadMovies()">
-          <option value="" >Genre</option>
-          <option value="comedy">Comedy</option>
-          <option value="romance">Romance</option>
-          <option value="horror">Horror</option>
-          <option value="drama">Drama</option>    
+          <option value="Genre" >Genre</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Romance">Romance</option>
+          <option value="Horror">Horror</option>
+          <option value="Drama">Drama</option>    
           
         </select> 
 </form>
 <br>
 </div>
-<h2>Original Array</h2>
+<!--<h2>Original Array</h2>
  <p id="movielist"></p>
  <br>
+
  <h2>Sorted Array</h2>
+--> 
  <p id="movielistsorted"></p>
 
 <script>
@@ -63,26 +65,30 @@
             
           
             var cboRating = document.getElementById('userRating');
-            var userRating = cboRating.options[cboRating.selectedIndex].text;
+            var userRating = cboRating.options[cboRating.selectedIndex].value;
             //window.alert(userRating);
             var cboGenre = document.getElementById('userGenre');
-            var userGenre = cboGenre.options[cboGenre.selectedIndex].text;
+            var userGenre = cboGenre.options[cboGenre.selectedIndex].value;
             var moviearray = '{ "movies" : [' +
                         '{ "name":"Star Wars" , "rating":"G" , "genre":"Comedy", "date":"April 19, 2006 18:44:20"},' +
-                        '{ "name":"Indiana Jones" , "rating":"PG" , "genre":"Romance", "date":"March 19, 2006 15:44:20"},' +
+                        '{ "name":"Indiana Jones" , "rating":"PG" , "genre":"Romance", "date":"January 19, 2006 15:44:20"},' +
                         '{ "name":"Indiana Jones 2" , "rating":"G" , "genre":"Romance", "date":"March 19, 2006 15:44:20"},' +
-                        '{ "name":"Indiana Jones 3" , "rating":"PG" , "genre":"Comedy", "date":"March 12, 2006 15:44:20"},' +
+                        '{ "name":"Indiana Jones 3" , "rating":"PG" , "genre":"Comedy", "date":"May 12, 2006 15:44:20"},' +
                         '{ "name":"Pride and Prejudice" , "rating":"PG-13" , "genre":"Drama", "date":"March 12, 2006 15:44:20"},' +
                         '{ "name":"Jumanji" , "rating":"PG" , "genre":"Horror", "date":"March 13, 2006 15:44:20"},' +
-                        '{ "name":"Back to the Future" , "rating":"R" , "genre":"Comedy", "date":"March 14, 2006 15:44:20"},' +
-                        '{ "name":"Gone with the Wind" , "rating":"Not Rated" , "genre":"Comedy", "date":"March 15, 2006 15:44:20"},' +
-                        '{ "name":"Terminator" , "rating":"R" , "genre":"Drama", "date":"March 16, 2006 15:44:20"},' +
-                        '{ "name":"My Wedding" , "rating":"Not Rated" , "genre":"Romance", "date":"March 17, 2006 15:44:20"},' +
+                        '{ "name":"Back to the Future" , "rating":"R" , "genre":"Comedy", "date":"December 14, 2006 15:44:20"},' +
+                        '{ "name":"Gone with the Wind" , "rating":"Not Rated" , "genre":"Comedy", "date":"April 15, 2006 15:44:20"},' +
+                        '{ "name":"Terminator" , "rating":"R" , "genre":"Drama", "date":"February 16, 2006 15:44:20"},' +
+                        '{ "name":"My Wedding" , "rating":"Not Rated" , "genre":"Romance", "date":"September 17, 2006 15:44:20"},' +
                         '{ "name":"Mission Impossible" , "rating":"PG-13" , "genre":"Drama", "date":"March 18, 2006 15:44:20"},' +
                         '{ "name":"Jaws" , "rating":"PG" , "genre":"Horror", "date":"November 1, 2007 18:44:20"} ]}';
                         var obj = JSON.parse(moviearray);
+                
+                
+                
+                       
                      //window.alert(moviearray);
-                    var origarray = "";
+                        var origarray = "";
                         var filteredobj = {"movies":[]};
                         for (var i = j = 0; i < obj.movies.length; i++) {
                             origarray += obj.movies[i].name + " - ";
@@ -90,10 +96,23 @@
                             origarray += obj.movies[i].genre + " - ";
                             origarray += obj.movies[i].date + "<br>";
 
-                             //add sorting code
-                                 
-                                filteredobj.movies[j++]=obj.movies[i];
-
+                            
+                                  
+                            filteredobj.movies[j++]=obj.movies[i];
+                            
+                            
+                     //sort by date if button2 is checked, otherwise sort Alphabetically     
+                        if (document.getElementById('button2').checked){
+                            
+                            filteredobj.movies = obj.movies.sort(sortByDate);
+                        }
+                        else{
+                            filteredobj.movies = obj.movies.sort(sortByName);
+                            
+                        }    
+                            
+                            
+                             
                         }
                             //puts sorted array into local storage
                         localStorage.setItem("sortedArray", JSON.stringify(filteredobj));
@@ -117,7 +136,7 @@
 
                                     }                         
                             }
-
+                            document.getElementById('userRating').options[0].text = "None";
                             localStorage.setItem("sortedArray", JSON.stringify(filteredobj));
                         }
                         
@@ -140,7 +159,7 @@
 
                                     }                         
                             }
-
+                            document.getElementById('userGenre').options[0].text = "None";
                             localStorage.setItem("sortedArray", JSON.stringify(filteredobj));
                         }
 
@@ -152,9 +171,25 @@
                             sortedarray += filteredobj.movies[i].genre + " - ";
                             sortedarray += filteredobj.movies[i].date + "<br>";
                         }
+                 function sortByName(a, b) {
+                       var sortStatus = 0;
+
+                        if (a.name < b.name) {
+                            sortStatus = -1;
+                        } else if (a.name > b.name) {
+                                sortStatus = 1;
+                        }
+                        return sortStatus;
+                        
+                    }
+                function sortByDate(a, b) {
+                        
+                        return new Date(b.date) - new Date(a.date);
+                    }    
+
+                        
                       
-                       
-                                document.getElementById("movielist").innerHTML = origarray;
+                                //document.getElementById("movielist").innerHTML = origarray;
 
                                 document.getElementById("movielistsorted").innerHTML = sortedarray;
                   
