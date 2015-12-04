8479e0015c530cb1474bb7cacc752bdcd1ba9247
VanSickle-Wendi <gwendells@gmail.com>
2015-12-03 20:22:46 -0700

    Delete all of my test pages

--- library/library_test.php
+++
@@ -1,6 +0,0 @@
-      <p id="test1"></p>
-      <p id="test2"></p>
-      <p id="test3"></p>
-<button type="button" onclick="clearDivs()">Example of hiding divs</button><br>
-
-
--- modules/nav_test.php
+++
@@ -1,6 +0,0 @@
-      <ul id="nav">
-        <li><a href="/test_home_page/home_page.php">Test Home</a></li>
-        <li><a href="/forms/add_movies_test.php">Add Movies</a></li>
-        <li><a href="/library/library_test.php">Movie Library</a></li>
-        <li><input type="text" placeholder="Search" ></li> 
-      </ul>
