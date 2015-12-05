9d7db6b29293133808e372cba0b4c40f26724349
Michael <sekimworld03@yahoo.com>
2015-12-04 20:26:50 -0800

    Fixed table reference

--- index.php
+++ index.php
@@ -2,42 +2,40 @@
 <html lang="en-us">
     <head>
         <title>Home | Movie App</title>
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/head.php'; ?>
     </head>
-    <body onload="load(); setup();">
-    <div class="container">        
-     <h1 class="title">Movie Library</h1> 
-    <!-- Navigation ------------------------------------------------>     
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/nav.php'; ?>
-    
-    <div class="mainContent">
-              
-    <!-- Content for Search results ---------------------------------->        
-     <div id="searchResults">               
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/search/search.php'; ?>
-     </div>   
-    
-     <!-- Content for Adding New Movies ------------------------------>  
-     <div id="addMovie">              
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/forms/add_movies.php'; ?> 
-     </div>  
-       
-     <!-- Content for viewing Movie Library -------------------------->
-     <div id="movieLibrary">     
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/library/library.php'; ?> 
-     </div>
-     
-     <!-- Content for Browsing Movie Library -------------------------->
-     <div id="browseMovies">     
-         <?php include $_SERVER['DOCUMENT_ROOT'].'/browse/browse.php'; ?> 
-     </div>     
-       
-     <p id="test3">Movies:</p>
-    
-    </div> <!-- End mainContent -->
-   </div> <!-- End Container -->        
-    <!-- Ensuring that Javascript runs ------------------------------->
-    <script type="text/javascript" src="/javascript/javascript.js"></script>  
-</body>
-</html>
+    <body onload="setup();">
+        <div class="container">        
+            <h1 class="title">Movie Library</h1> 
+            <!-- Navigation ------------------------------------------------>     
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/nav.php'; ?>
 
+            <div class="mainContent">
+
+                <!-- Content for Search results ---------------------------------->        
+                <div id="searchResults">               
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/search/search.php'; ?>
+                </div>   
+
+                <!-- Content for Adding New Movies ------------------------------>  
+                <div id="addMovie">              
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/forms/add_movies.php'; ?> 
+                </div>  
+
+                <!-- Content for viewing Movie Library -------------------------->
+                <div id="movieLibrary">     
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/library/library.php'; ?> 
+                </div>
+
+                <!-- Content for Browsing Movie Library -------------------------->
+                <div id="browseMovies">     
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/browse/browse.php'; ?> 
+                </div>     
+
+                <p id="movieTable">Movie Table:</p>
+
+            </div> <!-- End mainContent -->
+        </div> <!-- End Container -->        
+        <script>setup();</script>
+    </body>
+</html>
