9e23aa8ab04e2ed1eb2c49b67de1d8eb8a948680
tiffanygohnert <tiffanygohnert@gmail.com>
2015-12-09 10:21:10 -0800

    Update search.php

--- search/search.php
+++ search/search.php
@@ -1,13 +1,20 @@
 <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen"/>
 
-       <h2>Search Movies</h2>
-<!--creates input field(TG)-->
- <script type="text/javascript" src="javascript/javascript.js"></script>
        
+
+         <script type="text/javascript" src="javascript.js"></script>
+       
+<body>
+
+<h2>Search Movies</h2>
 <input type="text" id="search" value="Search">
 <button onclick="myFunction()">Search Your Movie Database</button>
 
 <p id="demo"></p>
+
+
+</body>
+
 
 
 
