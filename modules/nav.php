2a87e00b370c873cee56bacf4f8bbcf334109f91
tiffanygohnert <tiffanygohnert@gmail.com>
2015-12-09 09:18:49 -0800

    Update nav.php

--- modules/nav.php
+++ modules/nav.php
@@ -1,7 +1,10 @@
+      
+       <script type="text/javascript" src="javascript/javascript.js"></script>
       <ul id="nav2">
           <li><p onclick="clearDivsSearchResults()">Search Movies</p></li>
           <li><p onclick="clearDivsAddMovies()">Add Movies</p></li>
           <li><p onclick="clearDivsMovieLibrary()">My Library</p></li>
           <li><p onclick="clearDivsBrowseMovies()">Browse Movies</p></li>
-          <li><input type="text" placeholder="Search" ></li> 
+          <li><input type="text" id="search" value="Search">
+              <button onclick="myFunction()">Try it</button></li> 
       </ul>
