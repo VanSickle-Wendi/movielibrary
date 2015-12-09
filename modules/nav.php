4c557f04d9f43dba1a9d3851b3cc516466c9b44c
tiffanygohnert <tiffanygohnert@gmail.com>
2015-12-09 09:20:06 -0800

    Update nav.php

--- modules/nav.php
+++ modules/nav.php
@@ -6,5 +6,5 @@
           <li><p onclick="clearDivsMovieLibrary()">My Library</p></li>
           <li><p onclick="clearDivsBrowseMovies()">Browse Movies</p></li>
           <li><input type="text" id="search" value="Search">
-              <button onclick="myFunction()">Try it</button></li> 
+              <button onclick="myFunction()">Search Your Movie Database</button><p id="demo"></p></li> 
       </ul>
