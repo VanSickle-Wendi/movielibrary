9cbfe995f926d6433fc25aaf47aa7023ecad7c7f
VanSickle-Wendi <gwendells@gmail.com>
2015-12-09 15:22:14 -0700

    Larger Nav Icons / Corrected CSS for Add Movie form and search

--- modules/nav.php
+++ modules/nav.php
@@ -1,10 +1,10 @@
       
        <script type="text/javascript" src="javascript/javascript.js"></script>
       <ul id="nav">
-          <li><img src="/images/movie_library.png" onclick="clearDivsMovieLibrary()"></li>
-          <li><img src="/images/add_movie.png" onclick="clearDivsAddMovies()"></li>         
-          <li><img src="/images/browse.png" onclick="clearDivsBrowseMovies()"></li>
-          <li><img src="/images/search.png" onclick="clearDivsSearchResults()"></li>          
+          <li><img src="/images/movie_library2.png" onclick="clearDivsMovieLibrary()"></li>
+          <li><img src="/images/add_movie2.png" onclick="clearDivsAddMovies()"></li>         
+          <li><img src="/images/browse2.png" onclick="clearDivsBrowseMovies()"></li>
+          <li><img src="/images/search2.png" onclick="clearDivsSearchResults()"></li>          
       </ul>
             
        
