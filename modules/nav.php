f8f3259f4b894e40da1342bdffbbe54001693353
VanSickle-Wendi <gwendells@gmail.com>
2015-12-09 14:34:33 -0700

    Remove second nav bar, use icons only
    I can change this if you all don't agree to removing the search input
    field on the nav bar.

--- modules/nav.php
+++ modules/nav.php
@@ -3,16 +3,16 @@
       <ul id="nav">
           <li><img src="/images/movie_library.png" onclick="clearDivsMovieLibrary()"></li>
           <li><img src="/images/add_movie.png" onclick="clearDivsAddMovies()"></li>         
-          <li><img src="/images/search.png" onclick="clearDivsBrowseMovies()"></li>
+          <li><img src="/images/browse.png" onclick="clearDivsBrowseMovies()"></li>
           <li><img src="/images/search.png" onclick="clearDivsSearchResults()"></li>          
       </ul>
             
        
-       <ul id="nav2">
+ <!--      <ul id="nav2">
           <li><p onclick="clearDivsMovieLibrary()">My Library</p></li>
           <li><p onclick="clearDivsAddMovies()">Add Movies</p></li>          
           <li><p onclick="clearDivsBrowseMovies()">Browse Movies</p></li>          
           <li><p onclick="clearDivsSearchResults()">Search Movies</p></li>
           <li><input type="text" id="search" value="Search">
               <button onclick="myFunction()">Search</button><p id="demo"></p></li> 
-      </ul>
+      </ul> -->
