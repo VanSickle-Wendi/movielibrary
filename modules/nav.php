36559ba59ac60c6bafcfcf0e0e397bcfdbf694a7
VanSickle-Wendi <gwendells@gmail.com>
2015-12-10 20:45:23 -0700

    Add Transitions to Nav Bar to spin on hover

--- modules/nav.php
+++ modules/nav.php
@@ -6,13 +6,4 @@
           <li><img src="/images/browse2.png" onclick="clearDivsBrowseMovies()"></li>
           <li><img src="/images/search2.png" onclick="clearDivsSearchResults()"></li>          
       </ul>
-            
-       
- <!--      <ul id="nav2">
-          <li><p onclick="clearDivsMovieLibrary()">My Library</p></li>
-          <li><p onclick="clearDivsAddMovies()">Add Movies</p></li>          
-          <li><p onclick="clearDivsBrowseMovies()">Browse Movies</p></li>          
-          <li><p onclick="clearDivsSearchResults()">Search Movies</p></li>
-          <li><input type="text" id="search" value="" placeholder="Search">
-              <button onclick="myFunction()">Search</button><p id="demo"></p></li> 
-      </ul> -->
+
