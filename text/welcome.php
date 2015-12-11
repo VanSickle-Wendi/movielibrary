75b857e478470b1ac40ceed2edd6cad9e6ade7f6
VanSickle-Wendi <gwendells@gmail.com>
2015-12-11 01:22:22 -0700

    Changed JSON file to PHP to get rid of error in NetBeans

--- text/welcome.php
+++ text/welcome.php
@@ -1,3 +1,4 @@
 Welcome to Movie Library! You are seeing this message because it is your first visit or because there are no movies in your library.
 To begin use, please add your movies to the system through the add page. You can 
 then build your own personal library of movies.
+
